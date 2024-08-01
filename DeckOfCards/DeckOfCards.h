a124660f8c5f3e9f87dae054e0f9695fbbc485e5
Kiran Koneti <kiran.koneti@assurant.com>
2024-08-01 15:23:29 +0530

    Initial Commit

---
+++ DeckOfCards/DeckOfCards.h
@@ -0,0 +1,18 @@
+//
+//  DeckOfCards.h
+//  DeckOfCards
+//
+//  Created by Kiran_Koneti on 01/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeckOfCards.
+FOUNDATION_EXPORT double DeckOfCardsVersionNumber;
+
+//! Project version string for DeckOfCards.
+FOUNDATION_EXPORT const unsigned char DeckOfCardsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeckOfCards/PublicHeader.h>
+
+
